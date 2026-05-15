3d047fe72cce3a355f0390ad31f6fc676ded9112
AndréL.B.P <andre.l.pena@sou.unifeob.edu.br>
2026-05-15 20:26:03 -0300

    Update login_test.dart

--- integration_test/login_test.dart
+++ integration_test/login_test.dart
@@ -1,1 +1,33 @@
+// login_test.dart
 
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:integration_test/integration_test.dart';
+// import 'package:app_fluxolivre/main.dart' as app;
+//
+// void main() {
+//   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+//
+//   testWidgets('Teste de login', (tester) async {
+//     app.main();
+//     await tester.pumpAndSettle();
+//
+//     // Preenche email
+//     await tester.enterText(
+//       find.byType(TextField).at(0),
+//       'andre@email.com',
+//     );
+//
+//     // Preenche senha
+//     await tester.enterText(
+//       find.byType(TextField).at(1),
+//       '123456',
+//     );
+//
+//     // Faz login
+//     await tester.tap(find.text('Entrar'));
+//     await tester.pumpAndSettle();
+//
+//     // Verifica se entrou no sistema
+//     expect(find.text('Página Inicial'), findsOneWidget);
+//   });
+// }
