46721311b6523acd288e9fefbb8b1b3bfe8946a0
AndréL.B.P <andre.l.pena@sou.unifeob.edu.br>
2026-05-15 20:25:24 -0300

    Update app_test.dart

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,1 +1,17 @@
+// app_test.dart
 
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:integration_test/integration_test.dart';
+// import 'package:app_fluxolivre/main.dart' as app;
+//
+// void main() {
+//   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+//
+//   testWidgets('Verifica se o app inicia corretamente', (tester) async {
+//     app.main();
+//     await tester.pumpAndSettle();
+//
+//     // Verifica se a tela inicial aparece
+//     expect(find.text('FluxoLivre'), findsOneWidget);
+//   });
+// }
